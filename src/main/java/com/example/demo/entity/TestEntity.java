60de50455b55d2b85a4f4d218d2d8dd7da7f79c0
Saransh Agarwal <saransh808@gmail.com>
2020-09-17 22:54:48 +0530

    New Commit

---
+++ src/main/java/com/example/demo/entity/TestEntity.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+public class TestEntity {
+
+	private Integer primaryKey;
+	private String name;
+	public Integer getPrimaryKey() {
+		return primaryKey;
+	}
+	public void setPrimaryKey(Integer primaryKey) {
+		this.primaryKey = primaryKey;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
