c9f9958e87b0e0d9ede07e6ae5a5d25574a9119a
Saransh Agarwal <saranshagarwal@Saranshs-MacBook-Pro.local>
2020-11-13 22:04:22 +0530

    Added HTML response

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,11 +3,17 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import commonConstants.CommonHtmlResponse;
+
 @RestController
 public class TestController {
+	
+	
+	
+	private CommonHtmlResponse COMMON_HTML_RESPONSE;
 
 	@GetMapping("/")
 	public String getData() {
-		return "Success";  //SARANSH
+		return COMMON_HTML_RESPONSE.ROOT;
 	}
 }
